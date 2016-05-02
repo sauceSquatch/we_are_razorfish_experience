4252ff094dc488f0b542db8b881b23686c91347e
Jamie Naioti <jnaioti@usausrad8277.global.publicisgroupe.net>
2016-05-02 11:45:09 -0500

    updated so there is only one video active at a time, gets updated when state is updated. TODO: make 3 person objects to be animated and updated not instantly by state but rather when animation completes

--- src/components/PeopleView.js
+++ src/components/PeopleView.js
@@ -7,19 +7,37 @@ const PeopleView = React.createClass({
     peopleData: React.PropTypes.array.isRequired
   },
 
+  getInitialState: function() {
+      return {
+          currentId: 0
+      };
+  },
+
+  nextPerson: function() {
+
+    if(this.state.currentId >= (this.props.peopleData.length - 1)) {
+      this.activeId = 0
+    } else {
+      this.activeId = this.state.currentId + 1
+    }
+
+    this.setState(function(state) {
+      return {currentId: this.activeId};
+    });
+  },
+
   render() {
     return(
       <div className="people-display">
-        {this.props.peopleData.map( person => {
-          return (
-            <Person
-              key={person.id}
-              name={person.name}
-              title={person.title}
-              video={person.video}
-            />
-          )
-        })}
+        <h2 onClick={this.nextPerson}>Click me! currentId: {this.state.currentId}</h2>
+
+        <Person
+          key={this.props.peopleData[this.state.currentId].id}
+          name={this.props.peopleData[this.state.currentId].name}
+          title={this.props.peopleData[this.state.currentId].title}
+          video={this.props.peopleData[this.state.currentId].video}
+        />
+        
       </div>
     )
   }
